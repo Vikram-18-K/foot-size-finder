acd6a49553b06d7ea60955b12df9035279b0e903
Vikram K <vikramdivakar21@gmail.com>
2026-04-30 21:04:04 +0530

    Update API URL

--- config.js
+++ config.js
@@ -7,4 +7,4 @@
  * To switch to production, change the URL below to your Render backend:
  * e.g. "https://foot-scanner-api.onrender.com"
  */
-window.API_BASE_URL = 'http://192.168.43.157:8000';
+window.API_BASE_URL = 'https://foot-scanner-api.onrender.com';
